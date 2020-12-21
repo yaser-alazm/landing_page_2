223d492f8db0124bb26bd7cbc739c04245124af1
Yaser <yaser.azem@otmail.com>
2020-12-21 07:43:42 +0200

    adding youtube promotion video

--- src/components/hero/mainHero.tsx
+++ src/components/hero/mainHero.tsx
@@ -39,6 +39,11 @@ const MainHero = ({ minPrice }: Props) => (
 					<br />
 					(shared or dedicated) starting at just ${minPrice}
 				</p>
+				<Row>
+					<Col>
+						<iframe width="500" height="281" src="https://youtu.be/CmvXxNeu1_c" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen ng-show="showvideo"></iframe>
+					</Col>
+				</Row>
 				<a
 					href={MEMBERS_URL}
 					className="btn btn--green btn--lg mb-0 mt-4">
